4b13cc3b5b90d62abf9de6b8193c43d502c675c6
discreet <nurwary@gramedia.id>
2021-08-09 18:38:22 +0700

    New service of scaping result

--- web_scraping/views.py
+++ web_scraping/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import ScrapingResult
+
+
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome")
+
+
+def list_scraping(response):
+    ls = ScrapingResult.objects.all().values()
+    
+    return render(response, "list.html", {"ls":list(ls)})
