212f4723e84100a08bf7261acdec08f5809fda68
discreet <nurwary@gramedia.id>
2021-08-09 18:04:59 +0700

    Register some model with Inline relation

--- web_scraping/admin.py
+++ web_scraping/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.db import models
+from .models import ScrapingConfig, ScrapingResult, ScrapingConfigDetail, ImageResult
 
 # Register your models here.
+class ScrapingConfigDetailInline(admin.StackedInline):
+    model = ScrapingConfigDetail
+    extra = 3
+    max_num = 3
+
+class ScrapingConfigAdmin(admin.ModelAdmin):
+    inlines = [ScrapingConfigDetailInline,]
+
+admin.site.register(ScrapingConfig, ScrapingConfigAdmin)
+
+
+class ImageResultInline(admin.StackedInline):
+    model = ImageResult
+    extra = 0
+
+class ScrapingResultAdmin(admin.ModelAdmin):
+    inlines = [ImageResultInline,]
+
+    # Cannot add from admin
+    def has_add_permission(self, request, obj=None):
+        return False
+
+admin.site.register(ScrapingResult, ScrapingResultAdmin)
