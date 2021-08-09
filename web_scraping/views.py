5f9af82fa74ec47d92ceaa57b1fa05289c819b6b
discreet <nurwary@gramedia.id>
2021-08-09 20:42:56 +0700

    Call class scraping from helpers.py

--- web_scraping/views.py
+++ web_scraping/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from asgiref.sync import sync_to_async
 
 from .forms import ScrapingForm
 from .models import ScrapingResult
+from .helpers import ready_scraping, scraping
 
 
 # Create your views here.
@@ -20,7 +22,9 @@ def create_scraping(response):
     if response.method == "POST":
         f = ScrapingForm(response.POST)
         if f.is_valid():
-            f.save()
+            saving = f.save()
+            # sync_to_async(ready_scraping(saving), thread_sensitive=False)
+            scraping(saving)._process()
             return HttpResponseRedirect('/list')
     else:
         f = ScrapingForm()
